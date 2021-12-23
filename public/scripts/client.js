6b5c3336a371577bcb05e9452e701ac90d1de4ea
notlucaslee <lee.lucas.hyunguk@gmail.com>
2021-12-23 03:24:36 +0000

    add minor css improvements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//Take content from mock tweet database and create HTML tweet elements
+// const createTweetElements = function(tweet) {
+
+//   let $tweet = 
+//   `<article class="tweet-container">
+//   <header>
+//     <span>
+//       <img src="${tweet['user']['avatars']}" width="30px;" height="21.8px;">
+//       ${tweet["user"]["name"]}
+//     </span>
+//     <span>${tweet["user"]["handle"]}</span>
+//   </header>
+//   <p>${tweet["content"]["text"]}</p>
+  
+//   <footer>
+//     <span>${tweet["created_at"]}</span>
+//     <div class="icons">
+//     <i class="fa-regular fa-star"></i>
+//     <i class="fa-regular fa-heart"></i>
+//   </div>
+//   </footer>
+// </article>`
+
+// return $tweet;
+// }
+
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
